697fb3df2c234fba5227a3dffde075f58faa2f3b
Esther <esther@acceleratorapp.co>
2024-04-16 22:59:19 +0200

    project structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,7 @@
-"use client";
-import { useState } from "react";
-import Navbar from "./components/navbar";
-
-const Component = {
-  home: <div>home</div>,
-  about: <div>about</div>,
-  contact: <div>contact</div>,
-  projects: <div>projects</div>,
-};
-
 export default function Home() {
-  const [activePage, setActivePage] = useState<
-    "home" | "about" | "contact" | "projects"
-  >("home");
-  const ActiveComponent = () => Component[activePage];
   return (
     <main className="flex min-h-screen flex-col bg-white w-9/12 mx-auto">
-      <Navbar activePage={activePage} setActivePage={setActivePage} />
-      <ActiveComponent />
+      Project
     </main>
   );
 }
---
+++ components/header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <div>Logo</div>
+      <div>Nav</div>
+    </header>
+  );
+}
