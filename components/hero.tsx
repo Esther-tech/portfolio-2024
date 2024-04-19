518436541d29057bf156f4e3a9969549d2922091
Esther <esther@acceleratorapp.co>
2024-04-19 20:21:42 +0200

    setup main page

---
+++ components/hero.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div>
+      <div>
+        <h1>Hello, I am Esther</h1>
+        <p>Mid-level frontend developer</p>
+      </div>
+      <div>
+        <h2>Subtitle</h2>
+        <p>Short intro/about section</p>
+      </div>
+      <div>
+        <button>View resume</button>
+        <button>My projects</button>
+      </div>
+    </div>
+  );
+}
