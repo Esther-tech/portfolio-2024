884a98ec256731d207348e570d96a717c752e511
Esther <esther@acceleratorapp.co>
2024-05-08 18:56:05 +0200

    init darkmode

---
+++ components/darkMode.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { SunIcon, MoonIcon } from "@heroicons/react/16/solid";
+
+export default function DarkMode() {
+  return (
+    <button>
+      <SunIcon height={16} width={16} />
+    </button>
+  );
+}
