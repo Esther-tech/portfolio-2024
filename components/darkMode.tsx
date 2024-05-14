7b3fd336019a5446586b99fbbd65027351f54e7c
Esther <esther@acceleratorapp.co>
2024-05-14 17:59:07 +0200

    add dark mode functionality

--- components/darkMode.tsx
+++ components/darkMode.tsx
@@ -1,10 +1,35 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { SunIcon, MoonIcon } from "@heroicons/react/16/solid";
 
 export default function DarkMode() {
+  const localMode = window.localStorage.getItem("mode");
+  const darkPreference = window.matchMedia(
+    "(prefers-color-scheme: dark)"
+  ).matches;
+  const [darkMode, setDarkMode] = useState<boolean>(
+    () => localMode === "dark" || (!localMode && darkPreference) || false
+  );
+
   return (
-    <button className="fixed bottom-10 right-20 bg-slate-100 border border-slate-300 p-2 rounded-lg">
-      <SunIcon height={16} width={16} className="fill-slate-400" />
+    <button
+      className="fixed bottom-10 right-5 md:right-20 bg-white border border-slate-100 p-2 shadow-md rounded-lg"
+      onClick={() => {
+        const newMode = darkMode ? "light" : "dark";
+        setDarkMode(!darkMode);
+        window.localStorage.setItem("mode", newMode);
+        if (newMode === "dark") {
+          document.documentElement.classList.add("dark");
+        } else {
+          document.documentElement.classList.remove("dark");
+        }
+      }}
+    >
+      {darkMode ? (
+        <MoonIcon height={16} width={16} className="fill-slate-400" />
+      ) : (
+        <SunIcon height={16} width={16} className="fill-slate-400" />
+      )}
     </button>
   );
 }
